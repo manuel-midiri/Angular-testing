e36e916c3a7a0e8e693de85e86bc436b273d20bf
Manuel Midiri <manuel.midiri@hotmail.it>
2022-03-07 17:36:08 +0100

    Services testing

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,41 @@
+import { TestBed } from "@angular/core/testing";
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+describe('CalculatorService', () => {
+
+    let calculator: CalculatorService, loggerSpy: any;
+
+    beforeEach(() => {
+        console.log('Calling beforeEach');
+        
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        });
+        calculator = TestBed.get(CalculatorService);
+    });
+    it('should add two numbers', () => {
+        console.log('Calling add');
+
+        const result = calculator.add(2, 2);
+
+        expect(result).toBe(4);
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should subtract two numbers', () => {
+        console.log('Calling subtract');
+
+        const result = calculator.subtract(2, 2);
+
+        expect(result).toBe(0);
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+});
